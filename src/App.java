bf26de41c13dae8c40b7c713214ae95dfae096a5
JoshuaDadcoes <103644380+JoshuaDadcoes@users.noreply.github.com>
2022-04-18 17:52:35 +0700

    final-code

--- src/App.java
+++ src/App.java
@@ -1,5 +1,70 @@
 public class App {
+    /**
+     * This method takes in two int parameters to add.
+     * 
+     */
+    private static int sum2(int num1, int num2){
+        return num1 + num2;
+    }
+
+    /**
+     * This method takes in 3 int parameters to add.
+     * 
+     */
+    private static int sum3(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+    /**
+     * Modify the methods in such a way that the Class App
+     * can now have 2 overloaded computeSum() methods.
+     * One computing the sum of two numbers, and one computing the sum
+     * of three numbers. 
+     */
+    private static int computeSum(int num1, int num2){
+        return num1 + num2;
+    }
+    private static int computeSum(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+
+    /**
+     * 
+     * Create method that returns the greater integer value
+     */
+    private static int greaterValue(int a, int b){
+        if(a > b){
+            return a;
+        }else return b;
+    }
+
+    /**
+     * 
+     * Create method that returns the greater double value
+     */
+    private static double greaterValue(double x, double y){
+        if(x > y){
+            return x;
+        }else return y;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Create Method calls that takes in 2 parameters, and 3 parameters
+         * respectively and computes their sum.
+         */
+        System.out.println("Computing the sum of two numbers 10 and 20 = " + App.computeSum(10, 20));
+        System.out.println("Computing the sum of three numbers 10, 20, 30 = " + App.computeSum(10, 20, 30));
+        /**
+         * Call the greaterValue method with integer parameters
+         */
+        System.out.println("The greater value between 45 and 100 is " + greaterValue(45,100));
+
+        /**
+         * Call the greaterValue method with double parameters
+         */
+        System.out.println("The greater value between 3.14 and 9.8 is " + greaterValue(3.14,9.8));
+
     }
 }
